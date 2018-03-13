e3345f1469f40bd61b3921d779673615ef35ff83
Eivind Mjelde <eivindmjelde@gmail.com>
2018-03-13 22:03:48 +0100

    Gjort en del av gjennomgangen

--- source/_annotations/annotations.js
+++ source/_annotations/annotations.js
@@ -4,6 +4,11 @@
       "el": "#annotation-css-selector",
       "title" : "Annotation title",
       "comment": "Annotation description"
+    },
+    {
+      "el": ".c-paragraph",
+      "title" : "c-paragraph",
+      "comment": "This is the class that gives a paragraph its proper styling"
     }
   ]
 }
